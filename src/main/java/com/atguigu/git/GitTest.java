86a3f155ee63462c9a2f06cd993bb5a797030c7d
skz <925509492@qq.com>
2024-01-06 19:50:44 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello github");
         System.out.println("hello github3");
+        System.out.println("hello github4");
     }
 }
